6a49dfddcac2acf97c3197a505b050953eb24ced
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2025-05-16 08:37:53 +0700

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,5 @@
-import matplotlib.pyplot as plt
-
-frequencies = [200, 400, 600, 800, 1000]
-latencies = [0.5, 0.4, 0.35, 0.3, 0.25] # can be replaced by your actual measured latencies
-
-plt.plot(frequencies, latencies, marker='o')
-plt.title('CPU Frequency vs Latency')
-plt.xlabel('CPU Frequency (MHz)')
-plt.ylabel('Latency (seconds)')
-plt.grid()
-plt.show()
+#!/bin/bash
+for file in materials/*.py; do
+  echo "Running $file"
+  ./build/X86/gem5.opt "$file"
+done
